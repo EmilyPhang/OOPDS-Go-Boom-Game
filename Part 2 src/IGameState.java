62f192de9ea43f1ccd6f42228b4c99069e1fb8c7
Emily Phang <88608520+EmilyPhang@users.noreply.github.com>
2023-06-18 17:18:05 +0800

    Delete Part 2 src directory

--- Part 2 src/IGameState.java
+++
@@ -1,6 +0,0 @@
-public interface IGameState {
-    public void saveToFile(String filename);
-    public void loadToFile(String filename); 
-    public void deleteFile(String filename);
-
-}
--- Part 2 src/IRound.java
+++
@@ -1,7 +0,0 @@
-public interface IRound {
-    public void start();
-    public void createTrick();
-    public void startTrick();
-    public void endTrick();
-    
-}
